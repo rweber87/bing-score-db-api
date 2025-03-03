8a8094db649276ddcf50e746420c82de05a2c65f
Rob Weber <rob.weber87@gmail.com>
2025-03-03 11:44:38 +0100

    added migrations and update db config

---
+++ database/migrations/2025_03_03_102422_create_contests_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->date('started_at');
+            $table->date('ended_at');
+            $table->string('city');
+            $table->string('country');
+            $table->string('state')->nullable();
+        });
+    }
+};
